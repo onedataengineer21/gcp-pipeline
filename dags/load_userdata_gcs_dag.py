2207e8f1b3d781c3b41326d39a1f046d2502cccf
onedataengineer21 <onedataengineer@gmail.com>
2024-02-05 16:28:33 -0600

    Changing the DAG Schedule

--- dags/load_userdata_gcs_dag.py
+++ dags/load_userdata_gcs_dag.py
@@ -26,7 +26,7 @@ default_args = {
 
 dag = DAG(
     'load_userdata_gcs',
-    schedule_interval="*/20 * * * *",   # run every 2 minutes
+    schedule_interval="*/18 * * * *",   # run every 2 minutes
     max_active_runs=1,
     catchup=False,
     default_args=default_args

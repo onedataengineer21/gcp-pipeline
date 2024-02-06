c25544fcbed00a66393b6ba346160cb6b09e851b
onedataengineer21 <onedataengineer@gmail.com>
2024-02-05 20:56:44 -0600

    Changes to use config

--- dags/scripts/userdata_gcs_bq/load_userdata_gcs_bq.py
+++ dags/scripts/userdata_gcs_bq/load_userdata_gcs_bq.py
@@ -4,14 +4,21 @@ import pytz
 import requests
 import random
 import pandas as pd
+import yaml
 
-def extract_app_users_data():
+def load_config(config_path='configs/pipeline.yaml'):
+    """Load the pipeline configuration from a YAML file."""
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def extract_app_users_data(config):
     """
     Pulling randomuser information using the API
     With each call, we try to pull only 40-80 records randomly.
     """
-    url = f'http://randomuser.me/api?results={random.randint(40,80)}&nat=us,gb,in,es,ca,au'
-    result = requests.get(url)
+    url = config['api_settings']['api_path']
+    result = requests.get(f'{url}?results={random.randint(40,80)}&nat=us,gb,in,es,ca,au')
     result.raise_for_status()   # raise an exception if bad response returned
     data = result.json()
     users = pd.json_normalize(data["results"], max_level=1)
@@ -28,11 +35,12 @@ def transform(data):
     print("transform completed")
     return data
 
-def load_data_storage(data, foldername, filename):
+def load_data_storage(data, config, foldername, filename):
     """
     Writing the dataset to the cloud storage in the parquet format
     """
-    data.to_parquet(f'gs://api-composer-gcs-app-user-data/dt={foldername}/{filename}')
+    bucketname = config['gcs_settings']['gcs_bucket_name']
+    data.to_parquet(f'{bucketname}/dt={foldername}/{filename}')
     return None
 
 def load_data_bq(data):
@@ -42,7 +50,7 @@ def load_data_bq(data):
     # Construct a BigQuery client object.
     client = bigquery.Client()
 
-    table_id = "ecommerce_users_data.users_data"
+    table_id = config['bq_settings']['bq_table_id']
 
     job_config = bigquery.LoadJobConfig(
         schema=[
@@ -79,18 +87,20 @@ def app(filename, foldername):
     This is the main method in the job which calls the modules for completing the job
     """
     try:
+        config = load_config()
+
         ### Extracting the dataset from the API
-        data = extract_app_users_data()
+        data = extract_app_users_data(config)
 
         ### Transforming the dataset
         transformed = transform(data)
         transformed = transformed.astype('str')
 
         ### Loading the dataset to storage in parquet format
-        load_data_storage(transformed, foldername, filename)
+        load_data_storage(transformed, config, foldername, filename)
 
         ### Loading the dataset to bq
-        load_data_bq(transformed)
+        load_data_bq(transformed, config)
 
     except Exception as e:
             print(f"ERROR IN THE USER DATA EXTRACTION JOB: {e}")

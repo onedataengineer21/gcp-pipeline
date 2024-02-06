71e11266a38f7ce048b10502b4916e441e24b1fc
onedataengineer21 <onedataengineer@gmail.com>
2024-02-05 20:24:40 -0600

    Updating comments and adding yaml for configs

--- dags/scripts/userdata_gcs/load_userdata_gcs.py
+++ dags/scripts/userdata_gcs/load_userdata_gcs.py
@@ -1,13 +1,25 @@
 import requests
 import random
 import pandas as pd
+import yaml
 
-def extract_app_users_data():
-    url = f'http://randomuser.me/api?results={random.randint(40,80)}&nat=us,gb,in,es,ca,au'
-    result = requests.get(url)
+def load_config(config_path='configs/pipeline.yaml'):
+    """Load the pipeline configuration from a YAML file."""
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def extract_app_users_data(config):
+    """
+    Pulling randomuser information using the API
+    With each call, we try to pull only 40-80 records randomly.
+    """
+    url = config['api_settings']['api_path']
+    result = requests.get(f'{url}?results={random.randint(40,80)}&nat=us,gb,in,es,ca,au')
     result.raise_for_status()   # raise an exception if bad response returned
     data = result.json()
     users = pd.json_normalize(data["results"], max_level=1)
+    print("extract_app_users_data completed")
     return users
 
 def transform(data):
@@ -28,11 +40,13 @@ def load_data_storage(data, foldername, filename):
 
 def app(filename, foldername):
     """
-    This is the main method in the job which calls the modules for completing the job
+    This is the main method which calls the modules for completing the job
     """
     try:
+        config = load_config()
+
         ### Extracting the dataset from the API
-        data = extract_app_users_data()
+        data = extract_app_users_data(config)
 
         ### Transforming the dataset
         transformed = transform(data)
